52ce219b884b0925dff0ea3c59f7ab5782262a47
cassidy <casssmith1989@gmail.com>
2025-02-24 21:32:13 -0500

    Initial setup of Swagger documentation for Express microservices

--- finance/src/app.ts
+++ finance/src/app.ts
@@ -1,10 +1,16 @@
-import express, {Express, Request, Response} from 'express'
+import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
+import swaggerUi from 'swagger-ui-express'
+import YAML from 'yamljs'
 
 dotenv.config()
 
 const app: Express = express()
 const PORT = process.env.PORT ?? 4002
+
+const swaggerDocument = YAML.load('../swagger.yaml')
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Finance service running')
--- note/src/app.ts
+++ note/src/app.ts
@@ -1,10 +1,16 @@
 import express, {Express, Request, Response} from 'express'
 import dotenv from 'dotenv'
+import swaggerUi from 'swagger-ui-express'
+import YAML from 'yamljs'
 
 dotenv.config()
 
 const app: Express = express()
 const PORT = process.env.PORT ?? 4001
+
+const swaggerDocument = YAML.load('../swagger.yaml')
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Note service running')
